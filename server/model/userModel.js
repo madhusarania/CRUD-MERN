adfcb6e013b88477c608ed32e11c3ba339e558af
Madhu Sarania <madhusarania@gmail.com>
2024-11-13 22:30:57 +0530

    CRUD-mern

---
+++ server/model/userModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+});
+
+export default mongoose.model("Users", userSchema);
